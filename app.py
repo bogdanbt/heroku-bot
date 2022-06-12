a41093fb320043699b535883bc72b8697c1bfe84
bogdanbt <bt.tarasenko@gmail.com>
2022-06-12 16:19:07 +0300

    nice
    nice

--- app.py
+++ app.py
@@ -3,21 +3,41 @@ from flask import Flask, request
 from binance.client import Client
 api_keys='MJXjYzcIxaPjYELP5L6DLieRDhtcPkQndqqIy0aILO6b1pCIWcavtTf82RbAoNxx'
 api_secret='aDBRznWCHBQCzbALB3B1biPuyfuR9PDO2v9W03pJ8E2psO4fLDkvcBlWxIdB2NZq'
+
 def get_price(symbol):
-    price = client.get_avg_price(symbol=symbol)['price']
+    price = client.get_symbol_ticker(symbol=symbol)['price']
     return float(price)
 
-def get_coin(data):
-    symbol=data['symbol']
-    price=get_price(symbol)
-    usdt=int(data['usdt'])
-    coin=20*usdt/price
+def get_info_minsizecoin(symbol):
+    #https://stackoverflow.com/questions/71245810/find-tick-size-for-a-symbol-futures-binance//////////////
+    data = client.futures_exchange_info()
+    found = False
+    info = data['symbols']
+    for s in range(len(info)):
+        if info[s]['symbol'] == symbol:
+            filters = info[s]['filters']
+            for f in range(len(filters)):
+                if filters[f]['filterType'] == 'LOT_SIZE':
+                    #print(filters[f])
+                    tick_size = float(filters[f]['minQty'])
+                    found = True
+                    #print(tick_size)
+                    break
+            break
+    return tick_size
+    
+def value_after_dot(a):
+    b=int(a)
+    c=a-b
+    c=float(round(c,8)) # отсеим излишнюю точность
+    if c==0:
+        final=0
+        return final
+    else:
+        d=str(c)
+        final=len(d)-2
+        return final
 
-    if data['value']=="int":
-        coin=int(coin)
-    if data['value']=="float":
-        coin=float(coin)
-    return coin
 
 
 client=Client(api_key=api_keys, api_secret=api_secret,testnet=False)
@@ -38,9 +58,15 @@ def webhook():
     
     
     symbol=data['symbol']
-    quantity=get_coin(data)
-
-
+    price=get_price(symbol)
+    usdt=data['usdt']
+    coin=20*usdt/price
+    quantity = float(round(coin,8))
+    #print(quantity)
+    a=get_info_minsizecoin(symbol)
+    b=value_after_dot(a)
+    quantity=float(round(quantity,b))
+    #print(quantity)
     
     
 
